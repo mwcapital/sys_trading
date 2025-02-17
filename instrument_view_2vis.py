647e2163aff21a5b0cdcecebf504f98e9e9cf841
Maksim <maximishkulov@gmail.com>
2025-02-17 22:21:39 +0000

    Added/Updated requirements.py

--- instrument_view_2vis.py
+++ instrument_view_2vis.py
@@ -7,7 +7,7 @@ import plotly.figure_factory as ff
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.io as pio
-pio.renderers.default = "iframe"
+pio.renderers.default = "json"
 
 
 # Streamlit Layout
